880acaad2fcb0d4990eb070c3d9343d262539e62
jiangtao <jiangtao@wesai.com>
2020-08-11 17:01:08 +0800

    Update

--- main.d.ts
+++ main.d.ts
@@ -1,10 +1,9 @@
-import Vue from 'vue';
-
-class Component extends Vue {
-  static install(vue: typeof Vue): void
-}
-
 declare module "@jacker_tao/vue-scroll-view" {
+  import Vue from 'vue';
+
+  class Component extends Vue {
+    static install(vue: typeof Vue): void
+  }
   export default class ScrollView extends Component {
     limit: number;
     enabledUp: boolean;
